d3366c3fd913129fccae695594af139a159eb0ce
Harsh Patil <itsharshhh6@gmail.com>
2025-08-15 11:16:15 +0530

    Getting Started with Product Component to Set Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Products from './components/Products'
 
 function App() {
   
 
   return (
-    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-      Welcome!!!
-    </h1>
+    <>
+      <Products />
+    </>
   )
 }
 
