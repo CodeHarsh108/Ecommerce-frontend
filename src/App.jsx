07ae734e58fe09acbb7ec804f4b878f2422d0b1c
Harsh Patil <itsharshhh6@gmail.com>
2025-08-15 08:58:39 +0530

    Configuring dependencies in our project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+      Welcome!!!
+    </h1>
   )
 }
 
