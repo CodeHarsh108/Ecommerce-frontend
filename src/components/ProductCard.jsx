7b0e8637449e9f02a6ebfad1f51d6a910439e0c7
Harsh Patil <itsharshhh6@gmail.com>
2025-08-31 15:08:58 +0530

    Truncating description of the product

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 import ProductViewModal from './ProductViewModal';
 import React from 'react';
+import truncateText from './truncateText';
 
 
 const ProductCard = ({
@@ -55,7 +56,7 @@ const ProductCard = ({
                     {productName}
                 </h2>
                 <div className='min-h-20 max-h-20'>
-                    <p className='text-gray-600 text-sm'>{description}</p>
+                    <p className='text-gray-600 text-sm'>{truncateText(description, 80)}</p>
                 </div>
 
 
