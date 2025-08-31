6f3258f7d5dac0c436e932363965b8515a45377d
Harsh Patil <itsharshhh6@gmail.com>
2025-08-31 09:00:59 +0530

    Improving our Loader

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,6 +6,7 @@ import { fetchCategories, fetchProducts } from "../store/actions/index.js";
 import React from 'react';
 import Filter from "./Filter.jsx";
 import useProductFilter from "./useProductFilter.jsx";
+import { Hourglass } from "react-loader-spinner";
 
 const Products = () => {
     const { products, error, categories, categoryLoading } = useSelector((state) => state.products);
@@ -19,12 +20,39 @@ const Products = () => {
     console.log("Categories:", categories);
     console.log("Category loading:", categoryLoading);
 
+    // Loading state for categories
     if (categoryLoading) {
-        return <div className="text-center py-20">Loading categories...</div>;
+        return (
+            <div className="flex justify-center items-center py-20">
+                <Hourglass
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="hourglass-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    colors={['#306cce', '#72a1ed']}
+                />
+                <span className="ml-4 text-gray-600">Please Wait...</span>
+            </div>
+        );
     }
 
     if (!products && !error) {
-        return <div className="text-center py-20">Loading products...</div>;
+        return (
+            <div className="flex justify-center items-center py-20">
+                <Hourglass
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="hourglass-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    colors={['#306cce', '#72a1ed']}
+                />
+                <span className="ml-4 text-gray-600">Please Wait...</span>
+            </div>
+        );
     }
 
     if (error) {
