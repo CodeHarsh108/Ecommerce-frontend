7c8f7809d1eea81058d764415d719ab679543123
Harsh Patil <itsharshhh6@gmail.com>
2025-09-02 08:32:42 +0530

    Setting up routing for home and product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Products from './components/products/Products'
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   
 
   return (
-    <>
-      <Products />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/products' element={ <Products/> } />
+      </Routes>
+    </Router>
   )
 }
 
