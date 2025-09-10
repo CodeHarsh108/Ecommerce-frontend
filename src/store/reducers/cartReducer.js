82dff660c6365281b043041daad5697132f7ed75
Harsh Patil <itsharshhh6@gmail.com>
2025-09-10 20:55:37 +0530

    Cart

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,5 +1,3 @@
-import { act } from "react";
-
 const initialState = {
     cart: [],
     totalPrice: 0,
@@ -11,18 +9,19 @@ export const cartReducer = (state = initialState, action) => {
         case "ADD_CART":
             const productToAdd = action.payload;
             const existingProduct = state.cart.find((item) => item.productId === productToAdd.productId);
+            
             if (existingProduct) {
                 const updatedCart = state.cart.map((item) => {
                     if (item.productId === productToAdd.productId) {
                         return productToAdd;
-                    }else{
+                    } else {
                         return item;
                     }
                 });
-                 return{
+                return {
                     ...state,
                     cart: updatedCart,
-                 };
+                };
             } else {
                 const newCart = [...state.cart, productToAdd]; 
                 return {
@@ -30,10 +29,31 @@ export const cartReducer = (state = initialState, action) => {
                     cart: newCart,
                 };
             }
-            
-    
+
+        case "REMOVE_CART":
+            return {
+                ...state,
+                cart: state.cart.filter(
+                    (item) => item.productId !== action.payload.productId
+                ),
+            };
+
+        case "GET_USER_CART_PRODUCTS":
+            return {
+                ...state,
+                cart: action.payload,
+                totalPrice: action.totalPrice,
+                cartId: action.cartId,
+            };
+
+        case "CLEAR_CART":
+            return { 
+                cart: [], 
+                totalPrice: 0, 
+                cartId: null 
+            };
+
         default:
             return state;
     }
-    return state;
 }
